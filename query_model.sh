260003b8714f98024d3434fc9bed3e0d4a23beaa
KP <klmiltenbergerjr@gmail.com>
2025-12-10 17:24:06 -0800

    Update query_model.sh

--- query_model.sh
+++ query_model.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-# from windows, run chrome:  & "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222 --remote-allow-origins=* --remote-debugging-address=0.0.0.0
+# from windows: kill chrome then relaunch in debug mode
+# echo "killing your chrome!!!"
+# taskkill /F /IM chrome.exe
+# & "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222 --remote-allow-origins=127.0.0.1 --remote-debugging-address=0.0.0.0
 
 cd ~/fara
 python3 -m venv .venv
