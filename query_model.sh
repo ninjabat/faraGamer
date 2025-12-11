7a19624d9eda3abadf1b32e2317f797c634defa2
KP <klmiltenbergerjr@gmail.com>
2025-12-10 17:36:43 -0800

    Update query_model.sh

--- query_model.sh
+++ query_model.sh
@@ -5,7 +5,7 @@
 # & "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222 --remote-allow-origins=127.0.0.1 --remote-debugging-address=0.0.0.0
 
 cd ~/fara
-python3 -m venv .venv
-source .venv/bin/activate
+#python3 -m venv .venv
+#source .venv/bin/activate
 
 fara-cli --headful --task "whats the weather in new york now"
